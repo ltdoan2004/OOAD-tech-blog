68972308e1aab4712cc5c3f309f7c2f4ef83c5e5
ltdoan2004 <doanh25032004@gmail.com>
2024-11-30 12:18:31 +0700

    push config

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -3,9 +3,19 @@ import {makeSource, defineDocumentType} from '@contentlayer/source-files'
 
 const Blog = defineDocumentType(() => ({
     name: 'Blog',
-    filePathPattern: '**/*.mdx',
+    filePathPattern: '**/**/*.mdx',
     fields: {
         title: {type: 'string', required: true},
+        publishAt: {type: 'date', required: true},
+        publishAt: {type: 'date', required: true},
+        description: {type: 'string', required: true},
+        image: {type: 'string', required: true},
+        isPublished: {type: 'boolean', required: true},
+        author: {type: 'string', required: true},
+        tags: {type: 'string', required: true},
+    },
+    computedFields: {
+        url: {type: 'string', resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`},
     },
 }))
 
